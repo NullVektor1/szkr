81d631b1485e52a00fe1295cac465377df4ba2a0
tyukesz <nagy.szabolcs@sonrisa.hu>
2022-10-14 19:42:40 +0300

    Integrating router dom

--- src/layout/Header.js
+++ src/layout/Header.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from 'react-router-dom';
 import './Header.css';
 
 
@@ -9,13 +10,13 @@ const Header =  () => {
       <nav>
         <ul>
           <li>
-            <a href="/">Home</a>
+            <Link to="/">Home</Link>
           </li>
           <li>
-            <a href="/about">About</a>
+            <Link to="/about">About</Link>
           </li>
           <li>
-            <a href="/contact">Contact</a>
+            <Link to="/contact">Contact</Link>
           </li>
         </ul>
       </nav>
---
+++ src/pages/About.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+
+const About =  () => {
+
+  return (
+    <div>
+      <h1>About</h1>
+      <hr/>
+      <p>
+        This is the about page.
+      </p>
+    </div>
+  );
+}
+
+export default About;
